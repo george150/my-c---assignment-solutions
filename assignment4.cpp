ab82692950822f2fbaf19e619c2a8918b6eb54e4
george150 <georgeblack255@gmail.com>
2015-05-22 14:37:24 +0100

    MY C++ASSIGNMENT

---
+++ assignment4.cpp
@@ -0,0 +1,58 @@
+/*
+ *  Question4.cpp
+ *
+ *  Name:           [ GEORGE PETER OKORA]
+ *  Department:     [STATISTICS ]
+ *  Matric. No:     [ 45297108FA ]
+ *
+ *  Program that inputs a five-digit integer,
+ *  separates the integer into its digits and prints them separated by three spaces each
+ *
+ */
+ 
+ #include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "*********\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*********\n";
+	     cout << "   ** \n"
+	         << " *    * \n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << " *    *\n"
+	         << "   **  \n";
+	     cout << "  *   \n"
+	          << " ***  \n"
+	          << "***** \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n";
+	     cout << "    *   \n"
+	          << "   * *    \n"
+	         << " *     *  \n"
+	        << "*       *\n"
+	        << " *     * \n"
+	        << "   * *   \n"
+	        << "    *    \n";
+	        
+	        return 0;
+	        system("PAUSE");
+	        
+	        return 'EXIT_SUCCESS';
+}
